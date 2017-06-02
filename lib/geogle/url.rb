c464757f0109a9b21fcb8a845d7c34f4311a45c5
Gabriel Verdi <gverdi@devspark.com>
2017-06-02 10:54:52 -0300

    Fix error in comparison

--- lib/geogle/url.rb
+++ lib/geogle/url.rb
@@ -4,8 +4,8 @@ require 'dotenv'
 module Geogle
   module URL
     Dotenv.load
-    BASE       = ENV["GOOGLE_API"] | "https://maps.googleapis.com"
-    GEOCODE    = ENV["GOOGLE_API_GEOCODE"] | "#{BASE}/maps/api/geocode/json"
-    DIRECTIONS = ENV["GOOGLE_API_DIRECTIONS"] | "#{BASE}/maps/api/directions/json"
+    BASE       = ENV["GOOGLE_API"] || "https://maps.googleapis.com"
+    GEOCODE    = ENV["GOOGLE_API_GEOCODE"] || "#{BASE}/maps/api/geocode/json"
+    DIRECTIONS = ENV["GOOGLE_API_DIRECTIONS"] || "#{BASE}/maps/api/directions/json"
   end
 end
